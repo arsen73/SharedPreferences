5edc15598a687207942553787da1dd2560019b63
Arseniy Kharlanov <hab@albatrosscom.ru>
2016-01-31 22:35:49 +0300

    finish

--- src/android/pro/devapp/notifi/notification/Alarm.java
+++ src/android/pro/devapp/notifi/notification/Alarm.java
@@ -5,10 +5,19 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
 
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
-import pro.devapp.notifi.R;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * Created by arseniy on 31/01/16.
@@ -16,18 +25,62 @@ import pro.devapp.notifi.R;
 public class Alarm extends BroadcastReceiver {
 
     OkHttpClient client = new OkHttpClient();
+    Context context;
+    public static final MediaType MEDIA_TYPE_MARKDOWN
+            = MediaType.parse("application/json; charset=utf-8");
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        String login = PreferenceHelper.get(context.getApplicationContext(), "login", "");
-        if(!login.equals("")){
-            // Выбираем таблетки по которм нужно отправить уведомления
-            Notification notification = new Notification(context);
-            String title = context.getResources().getString(R.string.app_name);
-            notification.startNotification(title, login);
+        this.context = context;
+        String login = PreferenceHelper.get(context, "login", "empty");
+        Log.d(Alarm.class.getName(), login);
+        if(!login.equals("empty")){
+            TeskPush teskPush = new TeskPush();
+            teskPush.execute(login);
         }
     }
+
+
+    class TeskPush extends AsyncTask<String, String, String> {
+
+        String result = "";
+
+        @Override
+        protected String doInBackground(String... params) {
+            String url = PreferenceHelper.get(context, "server", "http://notpush.azurewebsites.net");
+            Request request = new Request.Builder()
+                    .url(url + "/api/PushMessages?employerLogin=" + params[0])
+                    .get()
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                result = response.body().string();
+            } catch (Exception e){
+                Log.d(Alarm.class.getName(), e.toString());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                Log.d(Alarm.class.getName(), result);
+                JSONObject resObj = new JSONObject(result);
+                Iterator itr = resObj.keys();
+                while (itr.hasNext()){
+                    String key = itr.next().toString();
+                    JSONArray item = resObj.getJSONArray(key);
+                    Notification notification = new Notification(context);
+                    //String title = context.getResources().getString(R.string.app_name);
+                    notification.startNotification(item.getString(1), item.getString(2));
+                }
+            } catch (Exception e){
+                Log.d(Alarm.class.getName(), e.toString());
+            }
+        }
+    };
 
     /**
      * Запускает проверку для уведомлений
@@ -39,7 +92,7 @@ public class Alarm extends BroadcastReceiver {
         Intent i = new Intent(context, Alarm.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
 
-        long s = 60000l; // 1m
+        long s = 10000l; // 1m
         long cm = System.currentTimeMillis() + 1000l; // add 10s
         cm = cm + (s - cm%s);
         am.setRepeating(AlarmManager.RTC_WAKEUP, cm, 1000 * 60, pi); // Millisec * Second * Minute
